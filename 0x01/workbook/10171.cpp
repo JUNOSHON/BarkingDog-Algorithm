7ebe1835ed1da98fe5a55804d0f216f950eb16e0
junoshon <sanoldfox@naver.com>
2023-04-26 16:51:10 +0900

    10171

---
+++ 0x01/workbook/10171.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout << "\\    /\\\n";
+  cout << " )  ( ')\n";
+  cout << "(  /  )\n";
+  cout << " \\(__)|\n";
+
+  /*
+  \    /\
+   )  ( ')
+  (  /  )
+   \(__)|
+  */
+}
